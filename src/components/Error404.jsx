2fb269bad1bd5cf4a1b8585a55521027c8bfd160
LuisReartez <luisreartez55@gmail.com>
2022-02-11 12:14:48 -0300

    se agrego la pagina error404

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
 import NavBar from './NavBar';
+import error404 from "./img/Error404.jpg" 
 const Error404 = () => {
     return (
         <div>
             <NavBar></NavBar>
-            <h1>Error 404</h1>
+            <div className=''>
+            <img src={error404} alt="imagen error404" width="100%" />
+            </div>
             <Footer></Footer>
         </div>
     );
